86c67a3983091aca85816f6145a2928e65e84f03
Venkata Sai <116882464+Venkata-Sai-Vishwanath-robo@users.noreply.github.com>
2024-04-22 16:19:34 +0530

    Update v1.c

--- v1.c
+++ v1.c
@@ -6,7 +6,7 @@ unsigned int flag2 = 0;
 #define EN_CTRL 0x10000000
 #define DT_CTRL 0x07800000
 
-LPC_GPIO0->FIODIR = 0x00000FF0;
+
 unsigned long int temp1=0,temp2=0,i,j;
 unsigned char flag1L=0,flag2L=0;
 unsigned char msg[]={"No of People:"};
@@ -19,8 +19,10 @@ unsigned int count = 0;
 
 
 
+
 int main()
 {
+LPC_GPIO0->FIODIR = 0x00000FF0;
     
 LPC_GPIO0->FIODIR=DT_CTRL | RS_CTRL | EN_CTRL;
 flag1L=0;
@@ -29,29 +31,30 @@ for (i=0;i<9;i++){
     lcd_write();
 }
 
+SystemInit();
+SystemCoreClockUpdate();
 
 while(1)
 {
-    SystemInit();
-    SystemCoreClockUpdate();
 
-    if(!(LPC_GPIO2->FIOPIN & (1<<12)) && flag1 == 0)
+
+    if((LPC_GPIO2->FIOPIN & (1<<12)) && flag1 == 0)
     {
         flag1 = 1;
     }
 
-    if(!(LPC_GPIO2->FIOPIN & (1<<13)) && flag1 == 1)
+    if((LPC_GPIO2->FIOPIN & (1<<13)) && flag1 == 1)
     {
         count = count + 1;
         flag2 = 1;
     }
 
-    if(!(LPC_GPIO2->FIOPIN & (1<<13)) && flag2 == 0)
+    if((LPC_GPIO2->FIOPIN & (1<<13)) && flag2 == 0)
     {
         flag2 = 1;
     }
 
-    if(!(LPC_GPIO2->FIOPIN & (1<<12)) && flag2 == 1)
+    if((LPC_GPIO2->FIOPIN & (1<<12)) && flag2 == 1)
     {
         count = count -1;
         flag1 = 1;
